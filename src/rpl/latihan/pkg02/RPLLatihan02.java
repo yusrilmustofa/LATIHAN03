49533c3be29ac81c489dd9a9da644b3dd73f59a1
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2019-08-22 14:17:17 +0700

    first commit

---
+++ src/rpl/latihan/pkg02/RPLLatihan02.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpl.latihan.pkg02;
+
+/**
+ *
+ * @author yusri
+ */
+public class RPLLatihan02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          int x=10;
+        int y=5;
+        double z =2.5;
+        
+        int a = 0;
+        int b = 0;
+        //Pembagian
+        a = x / y;
+        System.out.println("a="+a);
+        b=(int) (x / z);
+        System.out.println("b="+b);
+        
+    }
+    
+}
